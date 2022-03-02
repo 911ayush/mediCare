7f73d533017e5bffeb53ea2749b88667eabec42b
ayush <ayush911yadav@gmail.com>
2022-03-03 00:20:22 +0530

    ready

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,48 @@ mongoose.connect(process.env.DATABASE, {
 }).catch(err => {
     console.log(`error: ${err.message}`);
 });
+
+const server = require('../server');
+
+///////////////
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "https://medicare-ayusss.herokuapp.com",
+    },
+});
+
+let users = [];
+const addUser = (userid,socketid) => {
+    !users.some((user) => user.userid === userid) &&
+    users.push({ userid, socketid });
+    //console.log(users);
+}
+io.on('connection', (socket) => {
+
+
+    socket.on("adduser",(userid)=>{
+        addUser(userid,socket.id);
+        console.log("connected");
+    })
+
+    socket.on("send message",(data)=>{
+        console.log("send");
+        io.emit(data.to,data);
+    })
+
+    socket.on("disconnect", () => {
+        users.filter((users) => {
+            users.socketid !== socket.id
+        })
+       // console.log(users);
+    })
+
+})
+/////////////////////////////////
+
+
+
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
     console.log(`server listening at ${port}`);
